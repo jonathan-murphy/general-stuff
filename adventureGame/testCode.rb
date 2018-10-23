c150e40f8f8287dbac52d7bc1ea9231103582ff8
jonathan-murphy <jrmurphy7@gmail.com>
2018-10-23 21:22:24 +0100

    Rough card outline implemented

--- adventureGame/testCode.rb
+++ adventureGame/testCode.rb
@@ -11,10 +11,14 @@ Squib::Deck.new cards: data['name'].size, layout: layouts do
       layout: 'banner'
   svg file: data['continent'].map { |t| "#{t.downcase}.svg"},  # method 2
       layout: 'continent'
+  svg file: data['cost'].map { |t| "cost#{t}.svg"},  # method 2
+      layout: 'cost'
+  svg file: data['prestige'].map { |t| "prestige#{t}.svg"},  # method 2
+      layout: 'prestige'
   rect layout: 'cut'
   rect layout: 'safe'
   text str: data['name'], layout: 'title', font: 'Libre Baskerville, Bold', color: :textColor, stroke_color: :strokeColor, stroke_width: 3
-  text str: data['power'], layout: 'description'
+  text str: data['description'], layout: 'description', font: 'Daniel Black', color: :strokeColor
   #text str: Time.now, layout: 'credits'
   svg layout: data['faction']
   save_sheet prefix: 'testCards', columns: 4
